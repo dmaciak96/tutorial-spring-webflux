ef0461b59aa544249d0e00465dc8b1ca81a69615
Dawid <maciak.dawid.96@gmail.com>
2023-07-04 11:53:22 +0200

    Create R2DBC entity

---
+++ src/main/java/com/example/webfluxtutorial/domain/Beer.java
@@ -0,0 +1,27 @@
+package com.example.webfluxtutorial.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Beer {
+
+    @Id
+    private Integer id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
