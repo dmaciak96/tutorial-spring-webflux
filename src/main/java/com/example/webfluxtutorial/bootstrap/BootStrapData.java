ad6e7c1a382e9a5516e637bf499b427ac5f33353
Dawid <maciak.dawid.96@gmail.com>
2023-07-10 11:24:57 +0200

    Bootstrap data

---
+++ src/main/java/com/example/webfluxtutorial/bootstrap/BootStrapData.java
@@ -0,0 +1,58 @@
+package com.example.webfluxtutorial.bootstrap;
+
+import com.example.webfluxtutorial.domain.Beer;
+import com.example.webfluxtutorial.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BootStrapData implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    @Override
+    public void run(String... args) {
+        beerRepository.deleteAll()
+                .doOnSuccess(foo -> loadBeerData())
+                .subscribe();
+    }
+
+    private void loadBeerData() {
+        beerRepository.count().subscribe(count -> {
+            if (count == 0) {
+                var beer = Beer.builder()
+                        .beerName("Dzik")
+                        .beerStyle("IPA")
+                        .upc("123456222")
+                        .price(new BigDecimal("11.99"))
+                        .quantityOnHand(122)
+                        .build();
+
+                var beer2 = Beer.builder()
+                        .beerName("Harnaś")
+                        .beerStyle("Pale Ale")
+                        .upc("123123")
+                        .price(new BigDecimal("12.99"))
+                        .quantityOnHand(222)
+                        .build();
+
+                var beer3 = Beer.builder()
+                        .beerName("Warka")
+                        .beerStyle("IPA")
+                        .upc("123456333")
+                        .price(new BigDecimal("13.99"))
+                        .quantityOnHand(133)
+                        .build();
+                beerRepository.save(beer).subscribe();
+                beerRepository.save(beer2).subscribe();
+                beerRepository.save(beer3).subscribe();
+            }
+        });
+    }
+}
